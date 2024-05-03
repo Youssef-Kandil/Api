b94b95582d093040c6c686f5d6699c8300f835a3
Youssef_Kandil <116096005+Youssef-Kandil@users.noreply.github.com>
2024-05-03 10:41:07 +0300

    Update ConnectionClass.js

--- config/ConnectionClass.js
+++ config/ConnectionClass.js
@@ -5,11 +5,11 @@ class ConnectionClass {
     static async connectionFun(name){
         try{
           const  con =  ConnectionClass.connection = mysql.createConnection({
-                host: '192.168.1.15',
-                port:3307,
-                user:'root',
-                database:name,
-                password: '0000',
+                host: 'buylifjc78rygpv8lfhn-mysql.services.clever-cloud.com',
+                port:3306,
+                user:'ufvzxszrpjnqlp0z',
+                database:"ufvzxszrpjnqlp0z",
+                password: 'pnJuDwU8bv6TOLLAKGE7',
                 
             })
             
